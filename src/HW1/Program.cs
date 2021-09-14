c78fb61e0983cd6630dc0c3b7c6ec6cf42c7dd5f
ashblade <htcdnsnnkom2002@gmail.com>
2021-09-14 16:50:03 +0300

    2k-199 Error handling

--- src/HW1/Program.cs
+++ src/HW1/Program.cs
@@ -1,36 +1,42 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace HW1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string[] ValidOperations = new[] { "+", "-", "*", "/" };
+        static int Main(string[] args)
         {
-            var val1 = int.Parse(args[0]);
+            var isVal1Int = int.TryParse(args[0], out var val1);
             var operation = args[1];
-            var val2 = int.Parse(args[2]);
-            var result = 0;
-            switch (operation)
+            var isVal2Int = int.TryParse(args[2], out var val2);
+
+            if (!(isVal1Int && isVal2Int))
             {
-                case "+":
-                    result = val1 + val2;
-                    break;
-                case "-":
-                    result = val1 - val2;
-                    break;
-                case "*":
-                    result = val1 * val2;
-                    break;
-                case ":":
-                case "/":
-                    result = val1 / val2;
-                    break;
-                default:
-                    throw new ArgumentException();
+                Console.WriteLine($"{args[0]}{args[1]}{args[2]} are not a valid arguments");
+                return 1;
             }
 
+            if (!ValidOperations.Contains(operation))
+            {
+                Console.WriteLine($"{args[0]}{args[1]}{args[2]} are not a valid arguments "
+                                  + $"supported operations are {ValidOperations.Aggregate((c, n) => $"{c} {n}")}");
+                return 2;
+            }
+            var result = operation switch
+                         {
+                             "+" => val1 + val2,
+                             "-" => val1 - val2,
+                             "*" => val1 * val2,
+                             ":" => val1 / val2,
+                             "/" => val1 / val2,
+                             _   => 0
+                         };
+
             Console.WriteLine($"{val1}{operation}{val2}={result}");
+            return 0;
         }
     }
 }
