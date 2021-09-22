7caaedbac465a9ae5886f8b39ff57266e3b6b613
ashblade <htcdnsnnkom2002@gmail.com>
2021-09-22 15:40:33 +0300

    Add first test for calculator with simple data

--- tests/HW2.Tests/CalculatorTests.cs
+++ tests/HW2.Tests/CalculatorTests.cs
@@ -4,5 +4,21 @@ using Xunit;
 namespace HW2.Tests
 {
     public class UnitTest1
-    { }
+    {
+        [Theory]
+        [InlineData(2, "+", 2, 4)]
+        [InlineData(2, "/", 2, 1)]
+        [InlineData(2, "-", 2, 0)]
+        [InlineData(2, "*", 2, 4)]
+        public void Calculate_WithValidOperands_ShouldCalculateRight(int val1, string op, int val2, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = HW2.Calculator.Calculate(val1, op, val2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
 }
