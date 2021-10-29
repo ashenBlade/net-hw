36c18ff90e92f3a3a86c38635ee9d243d015920c
ashblade <htcdnsnnkom2002@gmail.com>
2021-10-29 09:08:34 +0300

    2k-379 Add custom extension method for IHtmlHelper<T> (without realization)

---
+++ src/HW7/Infrastructure/HtmlExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HW7.Infrastructure
+{
+    public static class HtmlExtensions
+    {
+        public static IHtmlContent FancyEditorForModel<T>(this IHtmlHelper<T> html)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
