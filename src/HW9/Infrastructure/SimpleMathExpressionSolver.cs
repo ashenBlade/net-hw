024af7cfa6a8b372dada40bd44435e7922d38394
ashblade <htcdnsnnkom2002@gmail.com>
2021-11-24 07:04:30 +0300

    2k-436 Remade calculation logic

--- src/HW9/Infrastructure/SimpleMathExpressionSolver.cs
+++ src/HW9/Infrastructure/SimpleMathExpressionSolver.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace HW9
@@ -8,31 +9,82 @@ namespace HW9
     public class SimpleMathExpressionSolver : ExpressionVisitor,
                                               IMathExpressionSolver
     {
-        protected override Expression VisitBinary(BinaryExpression node)
+        private Dictionary<Expression, Expression[]> ExpressionDependencies { get; } = new();
+        private Dictionary<Expression, decimal> Values { get; } = new();
+
+        private Dictionary<ExpressionType, int> Precedence { get; } = new()
+                                                                      {
+                                                                          {ExpressionType.Add, 1},
+                                                                          {ExpressionType.Subtract, 1},
+                                                                          {ExpressionType.Multiply, 2},
+                                                                          {ExpressionType.Divide, 2}
+                                                                      };
+
+        private int PrecedenceOf(ExpressionType type)
         {
-            var left = node.Left.Evaluate();
-            var right = node.Right.Evaluate();
-            Thread.Sleep(1000);
-            Task.WaitAll(left, right);
-            var leftValue = left.Result;
-            var rightValue = right.Result;
-            return node.NodeType switch
-                   {
-                       ExpressionType.Add      => Expression.Constant(leftValue + rightValue),
-                       ExpressionType.Subtract => Expression.Constant(leftValue - rightValue),
-                       ExpressionType.Multiply => Expression.Constant(leftValue * rightValue),
-                       ExpressionType.Divide   => Expression.Constant(leftValue / rightValue),
-                       _ => throw new
-                                InvalidOperationException($"Operation: {node.Method} not supported")
-                   };
+            return Precedence[type];
         }
 
+        private void SetDependencies(Expression parent, params Expression[] children)
+        {
+            ExpressionDependencies[parent] = children;
+        }
+
+        private Expression[] GetDependencies(Expression expression)
+        {
+            return ExpressionDependencies.TryGetValue(expression, out var dependencies)
+                       ? dependencies
+                       : Array.Empty<Expression>();
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            SetDependencies(node, node.Left, node.Right);
+            Visit(node.Left);
+            Visit(node.Right);
+            return node;
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            SetDependencies(node);
+            return node;
+        }
 
         public async Task<decimal> SolveAsync(Expression expression)
         {
-            return Visit(expression) is ConstantExpression constantExpression
-                       ? ( decimal ) constantExpression.Value
-                       : throw new Exception($"Invalid expression: {expression}");
+            Visit(expression); // Build dependency graph
+            return await SolveInnerAsync(expression);
+        }
+
+        private async Task<decimal> SolveInnerAsync(Expression expression)
+        {
+            await Task.WhenAll(GetDependencies(expression)
+                                  .Select(SolveInnerAsync)); // Solve dependencies
+            await SolveExpression(expression);               // Solve expression itself
+            return Values[expression];
+        }
+
+        private const int WaitTime = 1000;
+
+        private async Task SolveExpression(Expression expression)
+        {
+            if (expression is BinaryExpression binaryExpression)
+            {
+                var left = Values[binaryExpression.Left];
+                var right = Values[binaryExpression.Right];
+                await Task.Delay(WaitTime);
+                Values[expression] = binaryExpression.NodeType switch
+                                     {
+                                         ExpressionType.Add      => left + right,
+                                         ExpressionType.Subtract => left - right,
+                                         ExpressionType.Multiply => left * right,
+                                         ExpressionType.Divide   => left / right,
+                                         _                       => Values[expression]
+                                     };
+            }
+            else if (expression is ConstantExpression constantExpression)
+                Values[expression] = ( decimal ) constantExpression.Value;
         }
 
         public decimal Solve(Expression expression)
