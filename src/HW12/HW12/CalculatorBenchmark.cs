557a94e5f36d27775205c3912604244fccb3e685
ashblade <htcdnsnnkom2002@gmail.com>
2021-12-10 12:48:53 +0300

    2k-534 Add tests for each calculator method

--- src/HW12/HW12/CalculatorBenchmark.cs
+++ src/HW12/HW12/CalculatorBenchmark.cs
@@ -19,10 +19,11 @@ public class CalculatorBenchmark : IDisposable
     private WebApplicationFactory<StartUp.StartUp> _fSharpFactory;
     private HttpClient _fSharpClient;
 
-    private HttpRequestMessage CSharp1Plus1Message =>
-        new(HttpMethod.Get, "https://localhost:5001/Calculator/Add?left=1&right=1");
+    private HttpRequestMessage CSharp1123Plus1221Message =>
+        new(HttpMethod.Get, "https://localhost:5001/Calculator/Add?left=1123&right=1221");
 
-    private HttpRequestMessage FSharp1Plus1Message => new(HttpMethod.Get, "https://localhost:5001/add?v1=1&v2=1");
+    private HttpRequestMessage FSharp1123Plus1221Message =>
+        new(HttpMethod.Get, "https://localhost:5001/add?v1123=1&v2=1221");
 
     private static HttpRequestMessage GetMessage(string url)
     {
@@ -38,16 +39,70 @@ public class CalculatorBenchmark : IDisposable
         _fSharpClient = _fSharpFactory.CreateClient();
     }
 
-    [Benchmark(Description = "C# Server with 1 + 1")]
-    public void CSharp()
+    [Benchmark(Description = "C# 1123 + 1221")]
+    public void CSharpPlus()
     {
-        var r1 = _cSharpClient.SendAsync(CSharp1Plus1Message).GetAwaiter().GetResult();
+        var r1 = _cSharpClient.SendAsync(CSharp1123Plus1221Message).GetAwaiter().GetResult();
     }
 
-    [Benchmark(Description = "F# Server with 1 + 1")]
+
+    [Benchmark(Description = "F# 1123 + 1221")]
     public void FSharp()
     {
-        var r1 = _fSharpClient.SendAsync(FSharp1Plus1Message).GetAwaiter().GetResult();
+        var r1 = _fSharpClient.SendAsync(FSharp1123Plus1221Message).GetAwaiter().GetResult();
+    }
+
+    public HttpRequestMessage CSharp13Minus12Message =>
+        new(HttpMethod.Get, "https://localhost:5001/Calculator/Sub?left=13&right=12");
+
+
+    [Benchmark(Description = "C# 13 - 12")]
+    public void CSharpMinus()
+    {
+        var r1 = _cSharpClient.SendAsync(CSharp13Minus12Message).GetAwaiter().GetResult();
+    }
+
+    public HttpRequestMessage FSharp13Minus12Message => new(HttpMethod.Get, "https://localhost:5001/sub?v1=13&v2=12");
+
+    [Benchmark(Description = "F# 13 - 12")]
+    public void FSharpMinus()
+    {
+        var r1 = _fSharpClient.SendAsync(FSharp13Minus12Message).GetAwaiter().GetResult();
+    }
+
+
+    public HttpRequestMessage CSharp34Mul2Message =>
+        new(HttpMethod.Get, "https://localhost:5001/Calculator/Mul?left=34&right=2");
+
+    [Benchmark(Description = "C# 34 * 2")]
+    public void CSharpMul()
+    {
+        var r1 = _cSharpClient.SendAsync(CSharp34Mul2Message).GetAwaiter().GetResult();
+    }
+
+    public HttpRequestMessage FSharp34Mul2Message => new(HttpMethod.Get, "https://localhost:5001/mul?v1=34&v2=2");
+
+    [Benchmark(Description = "F# 34 * 2")]
+    public void FSharpMul()
+    {
+        var r1 = _fSharpClient.SendAsync(FSharp34Mul2Message).GetAwaiter().GetResult();
+    }
+
+    public HttpRequestMessage CSharp56Div2Message =>
+        new(HttpMethod.Get, "https://localhost:5001/Calculator/Div?left=56&right=2");
+
+    [Benchmark(Description = "C# 56 / 2")]
+    public void CSharpDiv()
+    {
+        var r1 = _cSharpClient.SendAsync(CSharp56Div2Message).GetAwaiter().GetResult();
+    }
+
+    public HttpRequestMessage FSharp56Div2Message => new(HttpMethod.Get, "https://localhost:5001/div?v1=56&v2=2");
+
+    [Benchmark(Description = "F# 56 / 2")]
+    public void FSharpDiv()
+    {
+        var r1 = _fSharpClient.SendAsync(FSharp56Div2Message).GetAwaiter().GetResult();
     }
 
     [GlobalCleanup]
@@ -55,5 +110,7 @@ public class CalculatorBenchmark : IDisposable
     {
         _cSharpClient?.Dispose();
         _cSharpFactory?.Dispose();
+        _fSharpFactory?.Dispose();
+        _fSharpClient?.Dispose();
     }
 }
