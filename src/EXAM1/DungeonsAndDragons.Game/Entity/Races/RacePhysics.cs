3d378c21fc63837e4a41ea008632cffebb6857d5
ashblade <htcdnsnnkom2002@gmail.com>
2022-01-09 13:10:35 +0300

    feat!: Remove Width from RacePhysics class

--- src/EXAM1/DungeonsAndDragons.Game/Entity/Races/RacePhysics.cs
+++ src/EXAM1/DungeonsAndDragons.Game/Entity/Races/RacePhysics.cs
@@ -2,15 +2,13 @@ namespace DungeonsAndDragons.Game.Entity.Classes;
 
 public class RacePhysics
 {
-    public RacePhysics(double height, double weight, double width, int speed)
+    public RacePhysics(double height, double weight, int speed)
     {
         Height = height;
         Weight = weight;
-        Width = width;
         Speed = speed;
     }
     public double Weight { get; private set; }
     public double Height { get; private set; }
-    public double Width { get; private set; }
     public int Speed { get; private set; }
 }
