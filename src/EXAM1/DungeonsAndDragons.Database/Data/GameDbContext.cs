b8e83781c9718fb77ebd76f179fad5f9632a1354
ashblade <htcdnsnnkom2002@gmail.com>
2022-01-09 15:44:16 +0300

    chore: Add DbSets to GameDbContext for main entities

--- src/EXAM1/DungeonsAndDragons.Database/Data/GameDbContext.cs
+++ src/EXAM1/DungeonsAndDragons.Database/Data/GameDbContext.cs
@@ -1,12 +1,17 @@
+using DungeonsAndDragons.Database.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace DungeonsAndDragons.Database.Data;
 
 public class GameDbContext : DbContext
 {
+    public DbSet<Armor> Armors { get; set; }
+    public DbSet<Class> Classes { get; set; }
+    public DbSet<Monster> Monsters { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Race> Races { get; set; }
+    public DbSet<Weapon> Weapons { get; set; }
     public GameDbContext(DbContextOptions<GameDbContext> options) 
         : base(options)
-    {
-        
-    }
+    { }
 }
