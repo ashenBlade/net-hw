7dedff6b6688bd4b702d4feabda9d44e743929b7
ashblade <htcdnsnnkom2002@gmail.com>
2022-01-09 13:08:54 +0300

    feat!: Remove Description property from Race class

--- src/EXAM1/DungeonsAndDragons.Game/Entity/Races/Race.cs
+++ src/EXAM1/DungeonsAndDragons.Game/Entity/Races/Race.cs
@@ -10,6 +10,5 @@ public class Race
         Name = name;
     }
     public string Name { get; private set; }
-    public string Description { get; private set; }
     public RacePhysics RacePhysics { get; private set; }
 }
