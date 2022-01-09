f72868eb6599a8510261aa93da742ca0f3e3728c
ashblade <htcdnsnnkom2002@gmail.com>
2022-01-09 13:08:17 +0300

    chore: Add constructor for Race class
    Name and race physics must be initialized

--- src/EXAM1/DungeonsAndDragons.Game/Entity/Races/Race.cs
+++ src/EXAM1/DungeonsAndDragons.Game/Entity/Races/Race.cs
@@ -4,6 +4,11 @@ namespace DungeonsAndDragons.Game.Entity.Races;
 
 public class Race
 {
+    public Race(RacePhysics racePhysics, string name)
+    {
+        RacePhysics = racePhysics;
+        Name = name;
+    }
     public string Name { get; private set; }
     public string Description { get; private set; }
     public RacePhysics RacePhysics { get; private set; }
