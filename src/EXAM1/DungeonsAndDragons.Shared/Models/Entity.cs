b134a5095ff20d43e71c357b74162e2d9428ab5e
ashblade <htcdnsnnkom2002@gmail.com>
2022-01-10 10:19:09 +0300

    chore(exam): Add constraint attributes

--- src/EXAM1/DungeonsAndDragons.Shared/Models/Entity.cs
+++ src/EXAM1/DungeonsAndDragons.Shared/Models/Entity.cs
@@ -1,13 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DungeonsAndDragons.Shared.Models;
 
 public class Entity
 {
+    [Required]
     public string Name { get; set; }
+    [Required]
     public int HitPoints { get; set; }
+    [Required]
     public int AttackModifier { get; set; }
+    [Required]
     public int AttackPerRound { get; set; }
+    [Required]
     public int Damage { get; set; }
+    [Required]
     public int DamageModifier { get; set; }
+    [Required]
     public int Weapon { get; set; }
+    [Required]
     public int ArmorClass { get; set; }
 }
