ab115e3c682b8f671f5a28194b7537c4b5466071
ash.blade <ashen.blade.dev@gmail.com>
2022-09-26 16:15:08 +0300

    redo aspnetcore project

--- src/backend/MessagesAPI/Controllers/WeatherForecastController.cs
+++ src/backend/MessagesAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MessagesAPI.Controllers;
 
@@ -19,7 +19,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet]
+    [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5)
--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {AMQPChannel, AMQPWebSocketClient} from "@cloudamqp/amqp-client";
 
 function App() {
+
+  const tls = window.location.protocol === "https:";
+  const url = `${tls ? "wss" : "ws"}://localhost:15670`
+  const amqp = new AMQPWebSocketClient(url, '/', 'guest', 'guest')
+
+  const input = useRef<HTMLInputElement>(null);
+  const textarea = useRef<HTMLTextAreaElement>(null);
+
+  async function start() {
+    try {
+      const conn = await amqp.connect()
+      const ch = await conn.channel()
+      attachPublish(ch)
+      const q = await ch.queue("");
+      await q.bind("amq.fanout");
+      const consumer = await q.subscribe({noAck: false}, (msg) => {
+        console.log(msg)
+
+        if (textarea.current) {
+          textarea.current.value += msg.bodyToString() + "\n";
+        }
+        msg.ack()
+      })
+    } catch (err) {
+      console.error("Error", err, "reconnecting in 1s")
+      disablePublish()
+      setTimeout(start, 1000)
+    }
+  }
+
+  function attachPublish(ch: AMQPChannel) {
+    document.forms[0].onsubmit = async (e) => {
+      e.preventDefault()
+      try {
+        await ch.basicPublish("amq.fanout",
+            "",
+            input.current?.value ?? '',
+            { contentType: "text/plain" })
+      } catch (err) {
+        console.error("Error", err, "reconnecting in 1s")
+        disablePublish()
+        setTimeout(start, 1000)
+      }
+
+    }
+  }
+
+  function disablePublish() {
+    document.forms[0].onsubmit = (e) => { alert("Disconnected, waiting to be reconnected"); }
+  }
+
+  useEffect(() => {
+    start();
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +67,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <textarea rows={10} ref={textarea} value={''}/>
+          <input ref={input}/>
+          <button type={'submit'}>Send</button>
+        </form>
       </header>
     </div>
   );
