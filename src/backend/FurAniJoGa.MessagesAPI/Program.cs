0fb15eee8fe054a1a1783feba564af6fad9115ba
ash.blade <ashen.blade.dev@gmail.com>
2022-10-01 21:00:10 +0300

    move listener to MessagesAPI

--- src/backend/FurAniJoGa.MessagesAPI/Program.cs
+++ src/backend/FurAniJoGa.MessagesAPI/Program.cs
@@ -2,25 +2,39 @@ using System.Text.Json;
 using FurAniJoGa.Domain;
 using FurAniJoGa.Infrastructure;
 using FurAniJoGa.Infrastructure.Managers;
+using MassTransit;
+using MessagesAPI.MessageQueue.Consumers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers().AddJsonOptions(json =>
 {
     json.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MessagesDbContext>(x =>
 {
-    x.UseNpgsql("User Id=postgres;Password=postgres;Host=database;Port=5432;Database=postgres");
+    x.UseNpgsql("User Id=postgres;Password=postgres;Host=localhost;Port=5432;Database=postgres");
 });
-builder.Services.AddScoped<IMessageManager, MessageManager>();
-
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<MessagePublishedConsumer>();
+    config.UsingInMemory((context, configurator) =>
+    {
+        configurator.Host();
+        configurator.ReceiveEndpoint(new TemporaryEndpointDefinition(), x =>
+        {
+            x.ConfigureConsumer<MessagePublishedConsumer>(context);
+        });
+        configurator.ConfigureEndpoints(context);
+    });
+});
+builder.Services.AddScoped<MessagePublishedConsumer>();
+builder.Services.AddScoped<IMessageFactory, SampleMessageFactory>();
+builder.Services.AddMassTransitHostedService();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- src/frontend/src/services/rabbitmqForumCommunicator.ts
+++ src/frontend/src/services/rabbitmqForumCommunicator.ts
@@ -24,63 +24,63 @@ export class RabbitmqForumCommunicator implements ForumCommunicator {
     }
 
     async open() {
-        this.amqp = new AMQPWebSocketClient(this.url, this.vhost, this.username, this.password);
-        this.client = await this.amqp.connect();
-        this.channel = await this.client.channel();
-        this.queue = await this.channel.queue(this.queueName);
-        await this.queue.bind(this.exchange, this.routingKey);
-        this.consumer = await this.queue.subscribe({}, (msg) => {
-            const body = msg.bodyToString();
-            if (body === null) {
-                console.error('Received body is null');
-                return;
-            }
-            let message: Message;
-            try {
-                message = RabbitMqMessagePublishedEvent.fromString(body).toMessage();
-            } catch (e) {
-                console.error('Could not parse received message', e);
-                return;
-            }
-
-
-            for (const callback of this.callbacks) {
-                try {
-                    callback(message);
-                } catch (e) {
-                    console.error('Error occurred in callback');
-                }
-            }
-        });
+        // this.amqp = new AMQPWebSocketClient(this.url, this.vhost, this.username, this.password);
+        // this.client = await this.amqp.connect();
+        // this.channel = await this.client.channel();
+        // this.queue = await this.channel.queue(this.queueName);
+        // await this.queue.bind(this.exchange, this.routingKey);
+        // this.consumer = await this.queue.subscribe({}, (msg) => {
+        //     const body = msg.bodyToString();
+        //     if (body === null) {
+        //         console.error('Received body is null');
+        //         return;
+        //     }
+        //     let message: Message;
+        //     try {
+        //         message = RabbitMqMessagePublishedEvent.fromString(body).toMessage();
+        //     } catch (e) {
+        //         console.error('Could not parse received message', e);
+        //         return;
+        //     }
+        //
+        //
+        //     for (const callback of this.callbacks) {
+        //         try {
+        //             callback(message);
+        //         } catch (e) {
+        //             console.error('Error occurred in callback');
+        //         }
+        //     }
+        // });
     }
 
     private async resetSubscribers() {
-        this.callbacks.length = 0;
+        // this.callbacks.length = 0;
     }
 
     unregisterOnMessageCallback(cb: MessageCallback): void {
-        const index = this.callbacks.indexOf(cb);
-        if (index !== -1) {
-            this.callbacks.splice(index, 1);
-        }
+        // const index = this.callbacks.indexOf(cb);
+        // if (index !== -1) {
+        //     this.callbacks.splice(index, 1);
+        // }
     }
 
     async close() {
-        await this.resetSubscribers();
-        if (this.amqp && !this.amqp.closed) {
-            await this.amqp.close()
-        }
+        // await this.resetSubscribers();
+        // if (this.amqp && !this.amqp.closed) {
+        //     await this.amqp.close()
+        // }
     }
 
     registerOnMessageCallback(cb: MessageCallback): void {
-        this.callbacks.push(cb);
+        // this.callbacks.push(cb);
     }
 
     async sendMessage(msg: Message): Promise<void> {
-        if (!this.channel) {
-            throw new Error('Channel is not opened')
-        }
-        const event = new RabbitMqMessagePublishedEvent(msg.username, msg.message);
-        await this.channel.basicPublish(this.exchange, this.routingKey, JSON.stringify(event));
+        // if (!this.channel) {
+        //     throw new Error('Channel is not opened')
+        // }
+        // const event = new RabbitMqMessagePublishedEvent(msg.username, msg.message);
+        // await this.channel.basicPublish(this.exchange, this.routingKey, JSON.stringify(event));
     }
 }
