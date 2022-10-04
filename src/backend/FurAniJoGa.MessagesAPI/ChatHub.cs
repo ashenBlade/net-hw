6fa0c5f9dae285c35d83304ffce853c15a6c119e
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-10-04 13:23:29 +0300

    [add] signalR on backend supporting

---
+++ src/backend/FurAniJoGa.MessagesAPI/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MessagesAPI;
+
+public class ChatHub : Hub
+{
+    public async Task PublishMessage(string username, string message)
+    {
+        await this.Clients.All.SendAsync("sendMessage",username, message);
+    }
+}
