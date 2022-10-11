9330f610b564a214f091e70148c4d536c8df740c
IHave3GitsAnd2Fridges <trololo1247@list.ru>
2022-10-11 18:15:23 +0300

    [add] extensions for services and middleware pipeline

---
+++ src/backend/FurAniJoGa.MessagesAPI/ExtensionMethods/ServicesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using FurAniJoGa.Domain;
+using FurAniJoGa.Infrastructure;
+using FurAniJoGa.Infrastructure.Repositories;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessagesAPI.ExtensionMethods;
+
+public static class ServicesExtensions
+{
+    public static void AddServicesForMessagesApi(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddControllers().AddJsonOptions(json =>
+        {
+            json.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddDbContext<MessagesDbContext>(x =>
+        {
+            var database = configuration["DB_DATABASE"] ?? throw new ArgumentNullException(null, "Database name is not provided");
+            var host = configuration["DB_HOST"] ?? throw new ArgumentNullException(null, "Database host is not provided");;
+            var username = configuration["DB_USERNAME"] ?? throw new ArgumentNullException(null, "Database username is not provided");;
+            var password = configuration["DB_PASSWORD"]  ?? throw new ArgumentNullException(null, "Database password is not provided");;
+            var portString = configuration["DB_PORT"] ?? throw new ArgumentNullException(null, "Database port is not provided");;
+            if (!int.TryParse(portString, out var port))
+            {
+                throw new ArgumentException($"Database port must be integer. Given: {portString}");
+            }
+
+            x.UseNpgsql($"User Id={username};Password={password};Host={host};Database={database};Port={port}");
+        });
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddSignalR();
+
+        services.AddMassTransit(configurator =>
+        {
+            throw new NotImplementedException("Implement connection to MassTransit pls");
+        });
+        services.AddScoped<IMessageFactory, SampleMessageFactory>();
+    }
+    
+}
--- src/backend/FurAniJoGa.MessagesAPI/Program.cs
+++ src/backend/FurAniJoGa.MessagesAPI/Program.cs
@@ -1,64 +1,11 @@
-using System.Text.Json;
-using FurAniJoGa.Domain;
-using FurAniJoGa.Infrastructure;
-using FurAniJoGa.Infrastructure.Repositories;
-using MassTransit;
-using MessagesAPI;
-using Microsoft.EntityFrameworkCore;
+using MessagesAPI.ExtensionMethods;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddJsonOptions(json =>
-{
-    json.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MessagesDbContext>(x =>
-{
-    var database = builder.Configuration["DB_DATABASE"] ?? throw new ArgumentNullException(null, "Database name is not provided");
-    var host = builder.Configuration["DB_HOST"] ?? throw new ArgumentNullException(null, "Database host is not provided");;
-    var username = builder.Configuration["DB_USERNAME"] ?? throw new ArgumentNullException(null, "Database username is not provided");;
-    var password = builder.Configuration["DB_PASSWORD"]  ?? throw new ArgumentNullException(null, "Database password is not provided");;
-    var portString = builder.Configuration["DB_PORT"] ?? throw new ArgumentNullException(null, "Database port is not provided");;
-    if (!int.TryParse(portString, out var port))
-    {
-        throw new ArgumentException($"Database port must be integer. Given: {portString}");
-    }
-
-    x.UseNpgsql($"User Id={username};Password={password};Host={host};Database={database};Port={port}");
-});
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-builder.Services.AddSignalR();
-
-builder.Services.AddMassTransit(configurator =>
-{
-    throw new NotImplementedException("Implement connection to MassTransit pls");
-});
-builder.Services.AddScoped<IMessageFactory, SampleMessageFactory>();
+builder.Services.AddServicesForMessagesApi(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors(x =>
-{
-    x.WithOrigins("http://localhost:8080");
-    x.AllowAnyHeader();
-    x.AllowAnyMethod();
-});
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapHub<ChatHub>("/chat");
+app.AddMiddlewaresForWebApplication();
 
 app.Run();
