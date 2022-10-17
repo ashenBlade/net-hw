8fc92ec4f1e7a0c4bb6c986777e87d2ebb974d5c
ash.blade <ashen.blade.dev@gmail.com>
2022-10-17 20:55:33 +0300

    add attachment field to Message interface

--- src/frontend/src/models/message.ts
+++ src/frontend/src/models/message.ts
@@ -1,6 +1,7 @@
 export interface Message {
     username: string
     message: string
+    attachment?: string
 }
 
 export function parseMessage(json: string): Message {
@@ -19,8 +20,13 @@ export function parseMessage(json: string): Message {
         throw new Error('Username must be string. Given: ', parsed.username);
     }
 
+    if (parsed.attachment && typeof parsed.attachment !== 'string') {
+        throw new Error('Attachment must be string or does not exist. Given: ', parsed.attachment)
+    }
+
     return {
         username: parsed.username,
-        message: parsed.message
+        message: parsed.message,
+        attachment: parsed.attachment
     }
 }
