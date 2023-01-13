9adfbaaa361ba10f4735ded7774d39e145f68a68
ash.blade <ashen.blade.dev@gmail.com>
2023-01-13 08:24:31 +0300

    обновил названия для конфигураций

--- src/backend/TicTacToe.Web/ConfigurationExtensions.cs
+++ src/backend/TicTacToe.Web/ConfigurationExtensions.cs
@@ -6,11 +6,10 @@ public static class ConfigurationExtensions
 {
     public static RabbitMqOptions GetRabbitMqOptions(this IConfiguration config)
     {
-        
         return new RabbitMqOptions()
                {
                    Host = config.GetValue<string>("RMQ_HOST"),
-                   Password = config.GetValue<string>("RMQ_PASS"),
+                   Password = config.GetValue<string>("RMQ_PASSWORD"),
                    Username = config.GetValue<string>("RMQ_USERNAME"),
                    Exchange = config.GetValue<string>("RMQ_EXCHANGE")
                };
